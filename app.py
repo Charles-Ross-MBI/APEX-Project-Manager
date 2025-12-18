cdce68d6dbd88438b6095f59e9c094dfe4128156
Charles Ross <113279548+CharlesCodyRoss@users.noreply.github.com>
2025-12-18 09:21:39 -0900

    Update app.py

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ elif st.session_state.step == 6:
     st.write("")
 
     # ✅ Back + Upload buttons appear together BEFORE upload starts
-    col_back, col_upload, _ = st.columns([1.2, 2.2, 0.6])   # wider upload column
+    col_back, col_upload = st.columns([1, 2])   # wider upload column
 
     if not st.session_state.get("upload_clicked", False):
 
